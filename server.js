84b14d0e6cf54fef317f65beafc35c251761ed80
LucFII <LSFIIChi@Gmail.com>
2017-08-17 17:18:22 -0500

    server updates

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 var express = require('express');
 var methodOverride = require('method-override');
 var bodyParser = require('body-parser');
+var mysql = require('mysql');
 
 var app = express();
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "burgerDB"
+  });
+  
+  connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+  });
