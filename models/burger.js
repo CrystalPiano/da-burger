71b259e564299f5ebee97c7fef0cded00c1e9c1a
LucFII <LSFIIChi@Gmail.com>
2017-08-19 11:44:42 -0500

    models update

--- models/burger.js
+++ models/burger.js
@@ -1,3 +1,25 @@
 var orm = require('../config/orm.js');
 
+module.exports = function(sequelize, DataTypes) {
+  var Post = sequelize.define("Post", {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    body: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      len: [1]
+    },
+    category: {
+      type: DataTypes.STRING,
+      defaultValue: "Personal"
+    }
+  });
+  return Post;
+};
+
 module.exports = burger;
